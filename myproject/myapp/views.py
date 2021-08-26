24854e12843d4c245aa9549d168cd35bfaaedf4e
gagan1147 <gagangautam1996@gmail.com>
2021-08-26 23:55:17 +0530

    search&home added

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -15,6 +15,68 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 # Create your views here.
+def home(request):
+    
+    try:
+        if (request.user.is_authenticated):
+            contact = Contacts.objects.filter(user = request.user)
+            context = {
+                "contacts":contact
+            }
+        return render(request,'home.html',context)
+    except:
+        form =  AuthenticationForm(request,data=request.POST or None)
+        if form.is_valid():
+            user_ = form.get_user()
+            login(request,user_)
+            return redirect("/")
+        context = {"form":form}
+        return render(request,'components/auth/login.html',{"form":form})
+
+def contact_view_api(request):
+    contexts = {}
+    contexts['Contacts'] = []
+    if (request.user.is_authenticated):
+        contacts = Contacts.objects.filter(user=request.user)
+        for contact in contacts:
+            jsontype = {
+                "firstName":contact.firstName,
+                "lastName":contact.lastName,
+                "email":contact.email,
+                "mobile":contact.mobileNumber
+            }
+            contexts["Contacts"].append(jsontype)
+    return JsonResponse(contexts)
+
+
+def search_view(request):
+    contexts = {}
+    contexts['Contacts'] = []
+    print(request.GET.get("searchValue"))
+    if(request.GET):
+        searchValue = request.GET.get("searchValue") 
+        try:
+            searchValue = int(searchValue)
+            contacts = Contacts.objects.filter(mobileNumber__icontains=searchValue,user= request.user)
+            for contact in contacts:
+                jsontype = {
+                    "firstName":contact.firstName,
+                    "lastName":contact.lastName,
+                    "email":contact.email,
+                    "mobile":contact.mobileNumber
+                }
+                contexts["Contacts"].append(jsontype)
+        except:
+            contacts = Contacts.objects.filter(firstName__icontains=searchValue,user= request.user)
+            for contact in contacts:
+                jsontype = {
+                    "firstName":contact.firstName,
+                    "lastName":contact.lastName,
+                    "email":contact.email,
+                    "mobile":contact.mobileNumber
+                }
+                contexts["Contacts"].append(jsontype)
+    return JsonResponse(contexts)
 
 def login_view(request):
     form =  AuthenticationForm(request,data=request.POST or None)
